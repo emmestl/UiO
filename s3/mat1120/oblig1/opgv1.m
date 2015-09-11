72e9c399104d0c030472e82c6544f22eec70246d
Elsie Mestl <emmestl@student.matnat.uio.no>
2015-09-11 14:02:54 +0200

    forhåpentligvis penere nå

--- s3/mat1120/oblig1/opgv1.m
+++ s3/mat1120/oblig1/opgv1.m
@@ -1,18 +1,25 @@
-P = [1 0.7 0 0 0 ; 0 0 0.5 0 0 ; 0 0.3 0 0.65 0 ; 0 0 0.5 0 0 ; 0 0 0 0.35 1];
+P = [1 0.7 0 0 0 ; 0 0 0.5 0 0
+    0 0.3 0 0.65 0 ; 0 0 0.5 0 0 ; 0 0 0 0.35 1];
 Pn = P;
 k = [3 4 40 80];
 
 for n = 1 : 4;
-    start = [0; 0; 0; 1; 0]; %har at alle utenom posisjon 4 er lik 0 siden vi vil vite hva som skjer om vi starter her   
+    start = [0; 0; 0; 1; 0]; %only s4 = 1, this is the state of intrest   
     for a = 1 : k(n); %antar vi sier k begynner paa 1
         
         Pn = P * Pn;
     end    
     
-    'Etter "a" tidsløp'
-    a
-    Pn
-    Pn*start
+    prob = Pn*start;
+    
+    fprintf('For n = %d\n ', a)
+    disp ('P = ')
+    disp (Pn)
+    
+    fprintf('get the followin matrix with start in s4 \n') 
+    disp(prob)
+    fprintf('probability to go from s4->s2 after n=%d is: %f \n',a, prob(2))
+    
 end
 
 
