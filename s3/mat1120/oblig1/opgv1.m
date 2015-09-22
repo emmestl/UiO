60b1cb5d869da485a59e818dba083c108fab97dc
Elsie Mestl <emmestl@student.matnat.uio.no>
2015-09-22 20:45:35 +0200

    nåååå

--- s3/mat1120/oblig1/opgv1.m
+++ s3/mat1120/oblig1/opgv1.m
@@ -1,11 +1,11 @@
 P = [1 0.7 0 0 0 ; 0 0 0.5 0 0
     0 0.3 0 0.65 0 ; 0 0 0.5 0 0 ; 0 0 0 0.35 1];
-Pn = P;
-k = [3 4 40 80];
+k = [2 3 4 40 80];
 
-for n = 1 : 4;
+for n = 1 : 5;
+    Pn = P;
     start = [0; 0; 0; 1; 0]; %only s4 = 1, this is the state of intrest   
-    for a = 1 : k(n); %start a = 1
+    for a = 2 : k(n); %start a = 1
         
         Pn = P * Pn;
     end    
@@ -18,7 +18,7 @@ for n = 1 : 4;
     
     fprintf('get the followin matrix with start in s4 \n') 
     disp(prob)
-    fprintf('probability to go from s4->s2 after n=%d is: %f \n',a, prob(2))
+    fprintf('probability to go from s4->s2 after n=%d is: %f \n\n\n',a, prob(2))
     
 end
 
