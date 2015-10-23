1445ffe158ad0d0301cccc1ba1ea71b0e6197225
Elsie Mestl <emmestl@student.matnat.uio.no>
2015-10-23 20:35:21 +0200

    hdkfjh

---
+++ s3/mat1120/oblig2/stdrot.m
@@ -0,0 +1,37 @@
+function rot = stdrot(p)
+    numtimes = 3000;
+    tol = 0.5 * 10^(-6);
+    x = ones(max(size(p))-1); %start columarray
+    x = x(:, 1);
+    rot = 0; %to stop matlab for complaining
+    
+    C = zeros(max(size(p))- 1, max(size(p)) - 1);
+
+    %going form polynom to matrix
+    for n = 1: max(size(p))-1
+        if n == max(size(p))-1
+            for m = 2: max(size(p))
+                C(n, max(size(p))-(m) +1) = - p(m);
+            end
+        else
+            C(n, n+1) = 1;   
+        end
+    end
+   
+    
+    %using the code given but changing the output
+    for n = 1: numtimes
+        x = C*x;
+        rot = max(abs(x));
+        x = (1/rot)*x;
+        error = max(abs(C*x-rot*x));
+    
+        if error < tol
+            break;
+        end
+    end
+    
+    if error > tol
+        disp('there is no dominant root in p')
+    end
+end
